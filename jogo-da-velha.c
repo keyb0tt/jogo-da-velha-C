1712c7da87d8958c4e8c3693306df7709fd87bc0
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-14 09:55:13 -0300

    .

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool winCondition(int somaX, int somaO){
-    if(somaX == 3 || somaO == 6){
+bool winCondition(int xScore, int oScore){
+    if(xScore == 3 || oScore == 6){
       return true;
     }
     return 0;
 }
 
 bool gameFinished(int matriz[3][3]){
-    int somaX = 0, somaO = 0;
+    int xScore = 0, oScore = 0;
     
     // Diagonal Principal
     for(int i = 0; i <= 2; i++){
         if(matriz[i][i] == 1){
-            somaX += matriz[i][i];
+            xScore += matriz[i][i];
         } else if(matriz[i][i] == 2){
-            somaO += matriz[i][i];
+            oScore += matriz[i][i];
         } else{
             break;
         }
-        printf("\nsomax=%d somao=%d", somaX, somaO);
+        printf("\nxScore=%d oScore=%d", xScore, oScore);
     }
 
-    if(winCondition(somaX, somaO)){
-        somaX = 0; somaO = 0;
+    if(winCondition(xScore, oScore)){
+        xScore = 0; oScore = 0;
         return true;
     }
     
@@ -32,17 +32,17 @@ bool gameFinished(int matriz[3][3]){
     int x = 2; 
     for(int i = 0; i <= 2; i++){
         if(matriz[i][x] == 1){
-            somaX += matriz[i][x];
+            xScore += matriz[i][x];
         } else if(matriz[i][x] == 2){
-            somaO += matriz[i][x];
+            oScore += matriz[i][x];
         } else {
             break;
         }
         x--;
     }
 
-    if(winCondition(somaX, somaO)){
-        somaX = 0; somaO = 0;
+    if(winCondition(xScore, oScore)){
+        xScore = 0; oScore = 0;
         return true;
     }
     
