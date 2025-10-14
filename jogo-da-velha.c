1be81cf033dda30c2cce890986edb6812349abc6
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-14 15:03:47 -0300

    .

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+struct Point 
+{
+    int x, y;
+}; 
+
 bool winCondition(int xScore, int oScore){
     if(xScore == 3 || oScore == 6){
       return true;
+    } else {
+        return false;
     }
+
     return 0;
 }
 
@@ -43,49 +51,49 @@ bool secondDiagonalScore(int gameState[3][3]){
 }
 
 bool columnScore(int gameState[3][3]){
-    int xScore = 0, oScore = 0;
-
-    for(int c = 0; c <= 2; c++){
-        for(int r = 0; r <= 2; r++){
-            if(gameState[r][c] == 1){
-                xScore += gameState[r][c];
-                if(xScore < 3 && r == 3){
-                    xScore = 0;
-                }
+    int xScore = 0, oScore = 0, row;
+    
+    for(int column = 0; column <= 2; column++){
+        xScore = 0, oScore = 0, row = 0;
+        do
+        {
+            if(gameState[row][column] == 1){
+                xScore += gameState[row][column];
             }
-            if(gameState[r][c] == 2){
-                oScore += gameState[r][c];
-                if(oScore < 6 && r == 3){
-                    oScore = 0;
-                } 
-            } else {
-                break;
-            }            
-        }
+            if(gameState[row][column] == 2){
+                oScore += gameState[row][column];
+            }
+            if(xScore == 3 || oScore == 6){
+                return winCondition(xScore, oScore);
+            }
+            row++;
+        } 
+        while(row <= 2);
     }
 
     return winCondition(xScore, oScore);
-}
+} 
+
 
 bool rowScore(int gameState[3][3]){
-    int xScore = 0, oScore = 0;
+    int xScore = 0, oScore = 0, column;
 
-    for(int r = 0; r <= 2; r++){
-        for(int c = 0; c <= 2; c++){
-            if(gameState[c][r] == 1){
-                xScore += gameState[c][r];
-                if(xScore < 3 && c == 3){
-                    xScore = 0;
-                }
-            } else if(gameState[c][r] == 2){
-                oScore += gameState[c][r];
-                if(oScore < 6 && c == 3){
-                    oScore = 0;
-                }
-            } else {
-                break;
+    for(int row = 0; row <= 2; row++){
+        xScore = 0, oScore = 0, column = 0;
+        do
+        {
+            if(gameState[row][column] == 1){
+                xScore += gameState[row][column];
             }
-        }
+            if(gameState[row][column] == 2){
+                oScore += gameState[row][column];
+            }
+            if(xScore == 3 || oScore == 6){
+                return winCondition(xScore, oScore);
+            }
+            column++;
+        } 
+        while(column <= 2);
     }
 
     return winCondition(xScore, oScore);
@@ -99,8 +107,27 @@ bool gameFinished(int gameState[3][3]){
     return 0;
 }
 
+struct Point moveConverter(int movement){
+    int counter = 0;
+    struct Point point;
+    point.x = 0, point.y = 0;
+
+    for(int r = 0; r <= 2; r++){
+        for(int c = 0; c <= 2; c++){
+            if(counter == movement){
+                point.x = r;
+                point.y = c;
+                return point;
+            }
+            counter++;
+        }
+    }        
+    return point;
+}
+
 void showTable(int gameState[3][3]){
     int counter = 0;
+
     for(int r = 0; r <= 2; r++){
         for(int c = 0; c <= 2; c++, counter++){
             if(gameState[r][c] == 1){
@@ -115,7 +142,6 @@ void showTable(int gameState[3][3]){
             if(c <= 1){
                 printf("|");
             }
-
         }
         printf("\n");
     }
@@ -125,16 +151,43 @@ int main(){
     int playerType;
     
     int gameState[3][3] = {
-        0, 0, 0,
-        0, 0, 0,
+        2, 1, 1,
+        1, 2, 1,
         0, 0, 0
-        // 1 == X; 2 == O; 0 == NULL;
+        // 1 == X; 0 == O; 0 == NULL;
     };
     
     printf("\n\n");
     showTable(gameState);
     printf("\n\n");
+    printf("\n\ncolumn:%d\n", columnScore(gameState));
+    printf("\nrow:%d\n", rowScore(gameState));
 
+    // int playerChoice;
+    // int counter = 0;
+    
+    // while(true){
+    //     printf("\n%d° Turno\n", counter + 1);
+    //     scanf("%d", &playerChoice);
+    
+    //     struct Point playerMovement = moveConverter(playerChoice);
+    //     if(gameState[playerMovement.x][playerMovement.y] != 0){
+    //         printf("\nMovimento já realizado\n");
+    //         showTable(gameState);
+    //         continue;
+    //     }
+    //     if(counter % 2 == 0){
+    //         gameState[playerMovement.x][playerMovement.y] = 1;
+    //     } else {
+    //         gameState[playerMovement.x][playerMovement.y] = 2;   
+    //     }
+    //     showTable(gameState);
+    //     if(gameFinished(gameState)){
+    //         return 0;
+    //     }
+
+    //     counter++;
+    // }
 
     return 0;
 }   
